a7a78903cb013f8c5cb178cc2d97bcb0fd4174b6
Anish Shanbhag <anish.shanbhag@yahoo.com>
2022-08-07 14:20:39 -0700

    ci: increase logs for debugging

--- packages/cli/test/utils.ts
+++ packages/cli/test/utils.ts
@@ -16,14 +16,16 @@ export function createTest(
     runCommand: (...args: Parameters<typeof runProcess>) => Promise<void>;
   }) => void | Promise<void>
 ) {
+  const start = Date.now();
   const name = path.basename(dir);
   const packageDir = path.join(dir, "package");
   const relativePath = (pathName: string) => path.join(packageDir, pathName);
   async function runCommand(...args: Parameters<typeof runProcess>) {
-    console.log("runCommand:", ...args);
+    console.log("runCommand:", Date.now() - start, ...args);
     await runProcess(args[0], args[1] ?? [], {
       cwd: packageDir,
       ...args[2],
+      fullOutput: true,
     });
   }
   describe(name, () => {
