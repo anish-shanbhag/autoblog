7e0225c4599364d340623083f0009618090c45e2
Anish Shanbhag <anish.shanbhag@yahoo.com>
2022-08-07 15:14:34 -0700

    chore(cli): remove logs in runProcess

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -116,13 +116,9 @@ export function runProcess(
   options: { cwd?: string; fullOutput?: boolean } = {}
 ): Promise<void> {
   const cwd = options.cwd ?? process.cwd();
-  console.log(command, args, options);
   if (!binPaths[cwd]) {
     binPaths[cwd] = execSync("npm bin", { cwd }).toString().trim();
   }
-  console.log("binPaths:", binPaths);
-  console.log(execSync("ls", { cwd }).toString());
-
   return new Promise<void>((resolve, reject) => {
     const childProcess = spawn(command, args ?? [], {
       shell: true,
@@ -133,8 +129,6 @@ export function runProcess(
         PATH: process.env.PATH + ";" + binPaths[cwd],
       },
     });
-    console.log("childProcess:", childProcess);
-
     let error = "";
     childProcess.stderr?.on("data", (data) => {
       error += data;
