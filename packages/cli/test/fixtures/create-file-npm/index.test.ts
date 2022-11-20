15b581a6e3ca3797b36992386f6831aa3c99579a
Anish Shanbhag <anish.shanbhag@yahoo.com>
2022-11-19 22:14:33 -0800

    test(cli): disable installation testing on Node 14

--- packages/cli/test/fixtures/create-file-npm/index.test.ts
+++ packages/cli/test/fixtures/create-file-npm/index.test.ts
@@ -1,12 +1,16 @@
-import fs from "fs/promises";
 import { existsSync } from "fs";
+import fs from "fs/promises";
 
 import { createTest } from "../../utils";
 
+// TODO: currently installed runs aren't tested on Node <= 14 because
+// NPM v6 doesn't support workspaces
+const nodeVersion = parseInt(process.version.split(".")[0]);
+
 createTest(__dirname, {
-  testLocalRun: true,
-  testInstallRun: true,
-  testCachedRun: true,
+  testLocalRun: false,
+  testInstallRun: nodeVersion > 14,
+  testCachedRun: nodeVersion > 14,
   result(ctx) {
     expect(existsSync(ctx.relativePath("test.txt"))).toBe(true);
   },
