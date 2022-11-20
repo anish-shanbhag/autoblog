938fa4349c26a4cc6016d8e72d18b9982656bbe5
Anish Shanbhag <anish.shanbhag@yahoo.com>
2022-11-19 21:20:40 -0800

    chore(bindings): update entry file with new name

--- packages/bindings/index.js
+++ packages/bindings/index.js
@@ -25,24 +25,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'rust-test.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'bindings.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./rust-test.android-arm64.node')
+            nativeBinding = require('./bindings.android-arm64.node')
           } else {
-            nativeBinding = require('rust-test-android-arm64')
+            nativeBinding = require('bindings-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'rust-test.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'bindings.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./rust-test.android-arm-eabi.node')
+            nativeBinding = require('./bindings.android-arm-eabi.node')
           } else {
-            nativeBinding = require('rust-test-android-arm-eabi')
+            nativeBinding = require('bindings-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -56,13 +56,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'rust-test.win32-x64-msvc.node')
+          join(__dirname, 'bindings.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./rust-test.win32-x64-msvc.node')
+            nativeBinding = require('./bindings.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('rust-test-win32-x64-msvc')
+            nativeBinding = require('bindings-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -70,13 +70,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'rust-test.win32-ia32-msvc.node')
+          join(__dirname, 'bindings.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./rust-test.win32-ia32-msvc.node')
+            nativeBinding = require('./bindings.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('rust-test-win32-ia32-msvc')
+            nativeBinding = require('bindings-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -84,13 +84,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'rust-test.win32-arm64-msvc.node')
+          join(__dirname, 'bindings.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./rust-test.win32-arm64-msvc.node')
+            nativeBinding = require('./bindings.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('rust-test-win32-arm64-msvc')
+            nativeBinding = require('bindings-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -103,12 +103,12 @@ switch (platform) {
   case 'darwin':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'rust-test.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'bindings.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./rust-test.darwin-x64.node')
+            nativeBinding = require('./bindings.darwin-x64.node')
           } else {
-            nativeBinding = require('rust-test-darwin-x64')
+            nativeBinding = require('bindings-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -116,13 +116,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'rust-test.darwin-arm64.node')
+          join(__dirname, 'bindings.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./rust-test.darwin-arm64.node')
+            nativeBinding = require('./bindings.darwin-arm64.node')
           } else {
-            nativeBinding = require('rust-test-darwin-arm64')
+            nativeBinding = require('bindings-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -136,12 +136,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'rust-test.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'bindings.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./rust-test.freebsd-x64.node')
+        nativeBinding = require('./bindings.freebsd-x64.node')
       } else {
-        nativeBinding = require('rust-test-freebsd-x64')
+        nativeBinding = require('bindings-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -152,26 +152,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'rust-test.linux-x64-musl.node')
+            join(__dirname, 'bindings.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./rust-test.linux-x64-musl.node')
+              nativeBinding = require('./bindings.linux-x64-musl.node')
             } else {
-              nativeBinding = require('rust-test-linux-x64-musl')
+              nativeBinding = require('bindings-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'rust-test.linux-x64-gnu.node')
+            join(__dirname, 'bindings.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./rust-test.linux-x64-gnu.node')
+              nativeBinding = require('./bindings.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('rust-test-linux-x64-gnu')
+              nativeBinding = require('bindings-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -181,26 +181,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'rust-test.linux-arm64-musl.node')
+            join(__dirname, 'bindings.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./rust-test.linux-arm64-musl.node')
+              nativeBinding = require('./bindings.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('rust-test-linux-arm64-musl')
+              nativeBinding = require('bindings-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'rust-test.linux-arm64-gnu.node')
+            join(__dirname, 'bindings.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./rust-test.linux-arm64-gnu.node')
+              nativeBinding = require('./bindings.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('rust-test-linux-arm64-gnu')
+              nativeBinding = require('bindings-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -209,13 +209,13 @@ switch (platform) {
         break
       case 'arm':
         localFileExisted = existsSync(
-          join(__dirname, 'rust-test.linux-arm-gnueabihf.node')
+          join(__dirname, 'bindings.linux-arm-gnueabihf.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./rust-test.linux-arm-gnueabihf.node')
+            nativeBinding = require('./bindings.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('rust-test-linux-arm-gnueabihf')
+            nativeBinding = require('bindings-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
